56d36dbae4be8f73e9ca0762f4daabcc6cb03b70
Steve <139585161+StevenWDowd@users.noreply.github.com>
2024-06-21 12:33:46 -0400

    Time: 58 ms (26.71%) | Memory: 47.6 MB (99.38%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.js
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.js
@@ -6,8 +6,6 @@ var largestAltitude = function(gain) {
     let currentHighest = 0;
     let height = 0;
     for (let i = 0; i < gain.length; i++){
-        // let currHeight = height + gain[i];
-        // if (currHeight > currentHighest) currentHighest = currHeight;
         height = height + gain[i];
         if (height > currentHighest) currentHighest = height;
     }
