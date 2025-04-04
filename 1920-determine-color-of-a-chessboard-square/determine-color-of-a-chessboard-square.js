6fd4753eb5d20373183c93e6ff158a3e90899ed3
Steve <139585161+StevenWDowd@users.noreply.github.com>
2025-04-04 04:57:35 -0400

    Time: 0 ms (100.00%) | Memory: 53 MB (62.07%) - LeetSync

---
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+var squareIsWhite = function(coordinates) {
+    const blackOdd = new Set(['a', 'c', 'e', 'g']);
+    //const blackEven = new Map(['b', 'd', 'f', 'h']);
+    let letter = coordinates[0];
+    let num = Number(coordinates[1]);
+    if (blackOdd.has(letter)){
+        if (num % 2 === 1) return false;
+        return true;
+    } else {
+        if (num % 2 === 0) return false;
+        return true;
+    }
+};
