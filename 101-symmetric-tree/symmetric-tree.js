a4bad21872083666f9b7229802953c17aab94948
Steve <139585161+StevenWDowd@users.noreply.github.com>
2025-09-15 21:44:02 -0400

    Time: 0 ms (100.00%) | Memory: 57.7 MB (29.51%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root) return true;
+    return mirrorCheck(root.left, root.right);
+};
+
+function mirrorCheck(tree1, tree2){
+    if (!tree1 && !tree2) return true;
+    if (!tree1 || !tree2) return false;
+    if (tree1.val !== tree2.val) return false;
+    return mirrorCheck(tree1.left, tree2.right) && mirrorCheck(tree1.right, tree2.left);
+}
