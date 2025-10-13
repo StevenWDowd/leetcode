3cf61fbca59e5da50ebced5c1d6c06b4076488f8
Steve <139585161+StevenWDowd@users.noreply.github.com>
2025-10-12 20:49:24 -0400

    Time: 296 ms (96.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+CASE
+    WHEN id % 2 = 0 THEN id - 1
+    WHEN id % 2 = 1 AND id in (SELECT MAX(id) FROM SEAT) THEN id
+    WHEN id % 2 = 1 THEN id + 1
+    END AS id,
+student
+FROM Seat
+ORDER BY id ASC;
