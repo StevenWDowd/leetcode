659e0ccbacd72f08a774cfed6ec6cccef030fed7
Steve <139585161+StevenWDowd@users.noreply.github.com>
2025-12-23 23:24:01 -0500

    Time: 2 ms (25.45%) | Memory: 56 MB (23.64%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.js
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.js
@@ -6,8 +6,6 @@
 var minimumBoxes = function(apple, capacity) {
     let appleNo = apple.reduce((acc, curr) => acc + curr, 0);
     let capSort = [...capacity].sort((a,b) => a-b);
-    console.log('capsort is: ', capSort);
-    console.log('appleno is: ', appleNo);
     let boxes = 0;
     while (appleNo > 0){
         let curr = capSort.pop();
